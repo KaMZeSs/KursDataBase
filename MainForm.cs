3c56ca71f5dbb33890a460706cc732000bcc1d8a
KaMZeSs <vladd267@gmail.com>
2022-04-04 13:54:58 +0300

    Initial commit

---
+++ MainForm.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+
+namespace KursDataBase
+{
+    public partial class MainForm : Form
+    {
+        private static String connString = "Host=localhost;Port=5432;User Id=postgres;Password=1310;Database=Kurs";
+        private NpgsqlConnection conn;
+        private String sql;
+        private NpgsqlCommand cmd;
+        private DataTable dt;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            conn = new NpgsqlConnection(connString);
+
+            var generate = new Randomize.Generate();
+
+            for (int i = 0; i < 5000; i++)
+            {
+                var city = generate.GetRandomCity();
+                var country = generate.GetCountry(city);
+                try
+                {
+                    conn.Open();
+                    sql = $"select country_id from Countries Where country_id = {country.id}";
+                    cmd = new NpgsqlCommand(sql, conn);
+                    if (cmd.ExecuteScalar() == null)
+                    {
+                        sql = $"select * from country_insert(:_id, :_name)";
+                        cmd = new NpgsqlCommand(sql, conn);
+                        cmd.Parameters.AddWithValue("_id", country.id);
+                        cmd.Parameters.AddWithValue("_name", country.Name);
+                        cmd.ExecuteScalar();
+                    }
+                    sql = $"select city_id from Cities Where city_id = {city.city_id}";
+                    cmd = new NpgsqlCommand(sql, conn);
+                    if (cmd.ExecuteScalar() == null)
+                    {
+                        sql = $"select * from city_insert(:_id, :_name, :coun_id)";
+                        cmd = new NpgsqlCommand(sql, conn);
+                        cmd.Parameters.AddWithValue("_id", city.city_id);
+                        cmd.Parameters.AddWithValue("_name", city.name);
+                        cmd.Parameters.AddWithValue("coun_id", city.country_id);
+                        cmd.ExecuteScalar();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error" + ex.Message);
+                }
+                finally
+                {
+                    conn.Close();
+                }
+            }
+            SelectAll("Cities");
+        }
+
+        private void SelectAll(String table)
+        {
+            try
+            {
+                conn.Open();
+                sql = $"select * from {table}";
+                cmd = new NpgsqlCommand(sql, conn);
+                dt = new DataTable();
+                dt.Load(cmd.ExecuteReader());
+                dataGridView1.DataSource = null; //очистка таблицы
+                dataGridView1.DataSource = dt;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error" + ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+    }
+}
---
+++ Randomize/Generate.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace KursDataBase.Randomize
+{
+    public class Generate
+    {
+        City[] cities;
+        Country[] countries;
+
+        public class City
+        {
+            public int city_id;
+            public int country_id;
+            public String name;
+        }
+        public class Country
+        {
+            public string Name;
+            public int id;
+        }
+
+        public Generate()
+        {
+            DeserialaizeCities();
+            DeserializeCounties();
+        }
+
+        private void DeserializeCounties()
+        {
+            var arrSerializer = new XmlSerializer(typeof(Country[]));
+
+            using (var reader = new StreamReader("Data/Countries.xml"))
+            {
+                countries = (Country[])arrSerializer.Deserialize(reader);
+            }
+        }
+        private void DeserialaizeCities()
+        {
+            var arrSerializer = new XmlSerializer(typeof(City[]));
+
+            using (var reader = new StreamReader("Data/Cities.xml"))
+            {
+                cities = (City[])arrSerializer.Deserialize(reader);
+            }
+        }
+        public City GetRandomCity()
+        {
+            return cities[new Random().Next(0, cities.Length)];
+        }
+        public Country GetCountry(City city)
+        {
+            return Array.Find(countries, (Country x) => x.id == city.country_id);
+        }
+    }
+}
